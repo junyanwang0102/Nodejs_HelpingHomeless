5ab91f90a2b1ba12c5c73429855f78bd3a796375
Andrew Wang <andrewwang0102@gmail.com>
2019-10-11 14:36:07 +0000

    delete the password

--- public/stylesheets/protect.js
+++ public/stylesheets/protect.js
@@ -1,13 +1,13 @@
-var pass1 = 'hello';
-console.log(document.cookie);
-if (document.cookie.split(';').filter(item => item.includes('password3=true')).length) {
-} else {
-	let password = prompt('Please enter your password to view this page!', '');
-	if (password === pass1) {
-		document.cookie = 'password3=true';
-		console.log(document.cookie);
-	}
-	if (password !== pass1) {
-		window.location = 'http://www.bringsmiles.tk';
-	}
-}
+// var pass1 = 'hello';
+// console.log(document.cookie);
+// if (document.cookie.split(';').filter(item => item.includes('password3=true')).length) {
+// } else {
+// 	let password = prompt('Please enter your password to view this page!', '');
+// 	if (password === pass1) {
+// 		document.cookie = 'password3=true';
+// 		console.log(document.cookie);
+// 	}
+// 	if (password !== pass1) {
+// 		window.location = 'http://www.bringsmiles.tk';
+// 	}
+// }
